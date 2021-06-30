0070c62bf77e4efa398b74fa3d1cdbeb265646e7
swimstatsapp <74018759+andrewlmao@users.noreply.github.com>
2021-06-30 08:34:42 -0700

    Revert "Added card functionality to the front page and web scraping capabilities"
    This reverts commit 749e9c55e8d6e9ea489b3ac8906eb48b532999e2.

--- lib/cardtemplate.dart
+++
@@ -1,6 +0,0 @@
-class SwimmerData {
-  String name = '';
-  String age = '';
-
-  SwimmerData({ required this.name, required this.age });
-}
--- lib/home.dart
+++ lib/home.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:swimstatsapp/cardtemplate.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
-  List<SwimmerData> swimmerList = [SwimmerData(name:'Andrew Luo',age:'17'), SwimmerData(name:'Marshall Yuan',age:'17')];
-  Widget cardTemplate(swimmer) {
-    return Card(
-      margin: EdgeInsets.all(20),
-      child: Column(
-        children: <Widget> [
-          Text(swimmer.name,
-          style:TextStyle(
-            fontSize: 20,
-            ),
-          ),
-          Text("Age: ${swimmer.age}"),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,17 +19,19 @@ class _HomeState extends State<Home> {
         body: Column(
           children: <Widget>[
             Container(
-              child:  Text('Click the button to add a swimmer')
+              child: Center(
+                child: Text('Click the button to add a swimmer'),
+              ),
             ),
-                ElevatedButton(
+            Container(
+              child: Center(
+                child: ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/swimmerinfo');
                   },
                   child: Text('Swimmer Info'),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: swimmerList.map((swimmer) => cardTemplate(swimmer)).toList(),
+                ),
+              ),
             ),
           ],
         ),
